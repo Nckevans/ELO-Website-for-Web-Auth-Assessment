9075cfda313530cc3f1b97293a07e1a5a515e3ec
NCKEvans <Nckevans@users.noreply.github.com>
2019-01-10 13:44:07 +0000

    Initial commit

---
+++ JS/JScript.js
@@ -0,0 +1,8 @@
+function login(){
+var x = document.getElementById("login").value;
+var y = "You are now logged in."
+alert(y);
+}
+ //console.log('message in console');
+ //alert('drop down box');
+ //prompt('box with text entry');
---
+++ JS/Store.js
@@ -0,0 +1,24 @@
+var total = 0;
+function shirt1(){
+	var t1 = total
+	var s1 = 25;
+	total = s1 + t1
+	alert("Item added to cart. You total is now £"+total)
+}
+
+function shirt2() {
+	var t2 = total
+	var s2 = 35;
+	total = t2 + s2
+	alert("Item added to cart. You total is now £"+total)
+}
+
+function mug1(){
+	var t3 = total
+	var m1 = 30;
+	total = m1 + t3
+	alert("Item added to cart. You total is now £"+total)
+}
+ //console.log('message in console');
+ //alert('drop down box');
+ //prompt('box with text entry');
